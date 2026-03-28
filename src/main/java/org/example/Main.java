f5b1b5220030aa1e00cd96d05771762c7ebc230b
Long <lluungoc22@gmail.com>
2026-03-28 16:20:25 +0700

    feat: sửa bug

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println("bai tạp lòn");
         System.out.println("Da tao nhanh dang nhap");
     }
+
 }
