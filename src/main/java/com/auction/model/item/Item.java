82c48ad5ba1884264566c449d07766cd23f1f629
Long <lluungoc22@gmail.com>
2026-03-31 22:41:51 +0700

    feat: hoàn thành Item

--- src/main/java/com/auction/model/item/Item.java
+++ src/main/java/com/auction/model/item/Item.java
@@ -1,4 +1,34 @@
 package com.auction.model.item;
 
 public class Item {
+    private String name;
+    private String description;
+    private double startingPrice;
+
+    public Item(String name, String  description, double startingPrice) {
+        this.name = name;
+        this.description = description;
+        this.startingPrice = startingPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description=description;
+    }
+
+    public double getStartingPrice() {
+        return startingPrice;
+    }
+    public void setStartingPrice(double startingPrice) {
+        this.startingPrice = startingPrice;
+    }
 }
