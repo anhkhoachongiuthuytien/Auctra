c658817fac5e7dda4b9c1ef4f6db414f4ba2691d
Minh dep zai khoai to <vuducminh3103@gmail.com>
2026-04-02 10:42:58 +0700

    feat: Dao, inMemoryDao

---
+++ src/main/java/com/auction/model/item/Art.java
@@ -0,0 +1,7 @@
+package com.auction.model.item;
+
+public class Art extends Item{
+    public Art(String id, String name, String description, double startingPrice){
+        super(id, name, description);
+    }
+}
